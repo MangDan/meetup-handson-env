040485e4f99e8b7525af564f46d3fe4b32540c1f
mangdan <dan.donghu.kim@gmail.com>
2020-01-27 13:05:52 +0900

    2020-01-27 13:05:52

--- meetup-handson-env-frontend/src/main.js
+++ meetup-handson-env-frontend/src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://140.238.22.91:8000'
+axios.defaults.baseURL = 'http://140.238.20.170:8000'
 //axios.defaults.baseURL = 'http://localhost:8000'
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.get["Content-Type"] = "application/json";
